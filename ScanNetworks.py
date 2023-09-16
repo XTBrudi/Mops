2bcef6566e5d622f75c3f22936a85c8df1cf1b3d
Z3US <132375404+XTBrudi@users.noreply.github.com>
2023-09-17 00:04:11 +0200

    Update ScanNetworks.py

--- ScanNetworks.py
+++ ScanNetworks.py
@@ -1,18 +1,17 @@
-import os
 import subprocess
 
 def scan_wifi_networks():
     try:
-        # Use subprocess to run the 'iwlist' command to scan for Wi-Fi networks
-        wifi_scan_results = subprocess.check_output(["iwlist", "wlan0", "scan"])
+        # Use subprocess to run the 'airport' command to scan for Wi-Fi networks
+        wifi_scan_results = subprocess.check_output(["airport", "-s"])
         wifi_scan_results = wifi_scan_results.decode("utf-8")
 
         # Extract and display the list of available Wi-Fi networks
         networks = []
         lines = wifi_scan_results.split("\n")
         for line in lines:
-            if "ESSID:" in line:
-                network = line.strip().split('"')[1]
+            if "SSID" in line:
+                network = line.strip().split(": ")[1]
                 networks.append(network)
 
         print("Available Wi-Fi networks:")
